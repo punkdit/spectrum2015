de6929cd9b5669891aa854423b48607a5060660c
Simon Burton <simon@arrowtheory.com>
2016-09-20 22:56:36 +1000

    commit edits

--- parselog.py
+++ parselog.py
@@ -21,6 +21,7 @@ for line in f:
 
 EPSILON = 1e-8
 n = len(rows)
+print len(rows), "rows"
 
 def lt(idxs, jdxs):
     assert idxs != jdxs
@@ -35,13 +36,13 @@ for i in range(n):
 
     if lt(rows[i][0], rows[j][0]):
         if rows[i][1] < rows[j][1]-EPSILON:
-            print rows[i]
-            print rows[j]
-            assert 0, (i, j)
+            print "%6d"%i, rows[i]
+            print "%6d"%j, rows[j]
+            print
+            #assert 0, (i, j)
+            assert sum(rows[i][0])>1
 
 
-print len(rows)
-print rows[-1]
 
 
 
