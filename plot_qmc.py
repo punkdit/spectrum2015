b6d0b3d141ef4651e6df21f36857fe51b9acaf51
Simon Burton <simon@arrowtheory.com>
2018-08-11 04:10:44 +1000

    commit edits

--- plot_qmc.py
+++ plot_qmc.py
@@ -2,9 +2,14 @@
 
 import sys, os
 
+import numpy
+from scipy import stats
+
 import matplotlib.pyplot as pyplot
 
-f = open(sys.argv[1])
+from argv import argv
+
+f = open(argv.next())
 ys = []
 xs = []
 i = 0
@@ -16,10 +21,37 @@ for line in f:
     ys.append(float(line))
     xs.append(i)
     i += 1
-        
 
-#pyplot.plot(xs, ys, 'bo')
-pyplot.plot(xs, ys, 'bo')
+if argv.plot:
+    pyplot.plot(xs, ys, 'bo')
+    pyplot.show()
+
+xs = numpy.array(xs)
+ys = numpy.array(ys)
+
+N = len(ys)
+        
+slope, intercept, r_value, p_value, std_err = stats.linregress(xs, ys)
+print("samples:", N)
+print("slope:", slope)
+print("total slope:", slope*N)
+
+ys -= ys.mean()
+
+print("range:", ys.min(), ys.max())
+
+rs = []
+for i in range(1, 200):
+
+    r = 0.
+    for j in range(0, N-i):
+        r += ys[j] * ys[j+i]
+    r /= N
+    rs.append(r)
+    print("%.0f"%(r/N), end=" ")
+print()
+
+pyplot.plot(rs)
 pyplot.show()
 
 
